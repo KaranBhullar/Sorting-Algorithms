d421f3a015f20c2cb6d08626b5c9ebe2200af86f
KaranBhullar <cswailord@gmail.com>
2023-05-02 16:06:17 -0400

    added selection sort file

---
+++ Selection Sort/SelectionSort.cpp
@@ -0,0 +1,5 @@
+#include "SelectionSort.h"
+
+void SelectionSort :: sort(int *arr) {
+    
+};
---
+++ Selection Sort/SelectionSort.h
@@ -0,0 +1,13 @@
+#ifndef SELECTIONSORT_H
+#define SELECTIONSORT_H
+
+#include <iostream>
+
+class SelectionSort {
+    public:
+        void sort(int*);
+    private:
+
+};
+
+#endif
