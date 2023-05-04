c57408647de97647df2fdf3048a4cbe608c6f225
KaranBhullar <cswailord@gmail.com>
2023-05-04 14:44:51 -0400

    added class and header files for Heap Sort

---
+++ Heap Sort/HeapSort.cpp
@@ -0,0 +1,13 @@
+#include "HeapSort.h"
+
+int HeapSort::heapsort(int* arr, int size) {
+
+}
+
+void HeapSort::buildMaxHeap() {
+
+}
+
+void HeapSort::heapify() {
+
+}
---
+++ Heap Sort/HeapSort.h
@@ -0,0 +1,13 @@
+#ifndef HEAPSORT_H
+#define HEAPSORT_H
+
+class HeapSort {
+    public:
+        int heapsort(int*, int);
+        void buildMaxHeap();
+        void heapify();
+    private:
+
+};
+
+#endif
